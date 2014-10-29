a455ca796a70e99731a09baf77129a16e6199490
Marc Boscher <boscher.marc@gmail.com>
2014-10-29 15:44:59 -0400

    Resolve #1 Add sync timer

--- cli.js
+++ cli.js
@@ -2,16 +2,28 @@
 
 'use strict';
 
-var crosscheck = require('./lib/crosscheck.js');
+var crosscheck = require('./lib/crosscheck.js'),
+  argv = require('minimist')(process.argv.slice(2)),
+  _ = require("lodash"),
+  interval;
 
-var userArgs = process.argv;
-
-if (userArgs.indexOf('-h') !== -1 || userArgs.indexOf('--help') !== -1) {
+if (argv.h || argv.help) {
     return console.log('Refer to https://github.com/marcboscher/crosscheck for help');
 }
 
-if (userArgs.indexOf('-v') !== -1 || userArgs.indexOf('--version') !== -1) {
+if (argv.v || argv.version) {
     return console.log(require('./package').version);
 }
 
+// Run once/a first time
 crosscheck.sync();
+
+// If requested, also run on interval
+if (argv.t || argv.timer) {
+  interval = argv.t ? argv.t : argv.timer;
+  if (!_.isNumber(interval)) {
+    return console.log("Time interval '%s' is not a number", interval);
+  }
+  interval *= 1000; // convert to ms
+  setInterval(crosscheck.sync, interval);
+}
