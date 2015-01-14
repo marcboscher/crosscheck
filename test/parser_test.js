ef545b24614c5752983bf17b5d258bdf3cbe6e77
Marc Boscher <boscher.marc@gmail.com>
2015-01-14 10:44:46 -0500

    Improve parser to be more resilient to false fields for Support hash sign as part of markdown

--- test/parser_test.js
+++ test/parser_test.js
@@ -22,17 +22,34 @@ describe("parser.", function () {
       var fields = {
           "extra-field": "bar"
         },
-        inputText = "#field1 value\nline 1\n#field2   \n\n #field3 value of field 3\n# extract space after separator --> no field\nline 2 \nover multiple\n lines\n#! value of single character field\nline 3 with trailing white space   \n#final-field.at/end value of field at end",
+        inputText = "#field1 value\nline 1\n#field2   \n\n#field3 value of field 3\nline 2 \nover multiple\n lines\n#a value of single character field\nline 3 with trailing white space   \n#final-field.at/end value of field at end",
         outputText,
-        expectedText = "line 1\n\n# extract space after separator --> no field\nline 2 \nover multiple\n lines\nline 3 with trailing white space   ",
+        expectedText = "line 1\n\nline 2 \nover multiple\n lines\nline 3 with trailing white space   ",
         expectedFields = {
           "extra-field": "bar",
           "field1": "value",
           "field2": "",
           "field3": "value of field 3",
-          "!": "value of single character field",
+          "a": "value of single character field",
           "final-field.at/end" : "value of field at end"
         };
+      
+      outputText = parser.extractFields(inputText, fields);
+      //console.log(outputText);
+      outputText.should.be.exactly(expectedText);
+    });
+  });
+
+  describe("extractFields", function () {
+    it("should ignore lines that don't start with # + letter", function () {
+      var fields = {},
+        inputText = "prefix\n" +
+          "##ignore value\n## ignore\n###ignore\n####ignore \n##### ignore \n" +
+          " #ignore\n!ignore\n#1ignore\n#%ignore\n# ignore" +
+          "suffix",
+        outputText,
+        expectedText = inputText,
+        expectedFields = {};
       
       outputText = parser.extractFields(inputText, fields);
       //console.log(outputText);
